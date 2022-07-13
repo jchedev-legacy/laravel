8b7fb83b01de0112c7d348a1123c625f52ebc5dd
Jean Francois Chedeville <jef.chedeville@gmail.com>
2022-07-13 13:00:35 -0700

    With formatting

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
+            input:   [
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
